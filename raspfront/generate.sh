a0428d18c39b747667b171cfe2946fd02cd1f219
stephane apiou <stephane@apiou.org>
2024-02-18 23:58:16 +0100

    corrected generation script

--- raspfront/generate.sh
+++ raspfront/generate.sh
@@ -13,7 +13,8 @@
 set -x
 asciidoctor -r asciidoctor-diagram -a toc -a data-uri -a allow-uri-read -a source-highlighter=rouge -b html raspfront_installation.asc
 asciidoctor-pdf -r asciidoctor-diagram -a toc -a data-uri -a allow-uri-read -a source-highlighter=rouge -d book raspfront_installation.asc
-asciidoctor-epub3 -r asciidoctor-diagram -a toc -a allow-uri-read  -b docbook raspfront_installation.asc
+asciidoctor-epub3 -r asciidoctor-diagram -a toc -a allow-uri-read  raspfront_installation.asc
+asciidoctor -r asciidoctor-diagram -a toc -a allow-uri-read -a source-highlighter=rouge -b docbook raspfront_installation.asc
 pandoc -f docbook -t rst raspfront_installation.xml -o raspfront_installation.tmp
 echo '
 .. toctree::
